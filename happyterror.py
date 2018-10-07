5dd9296ef29f2fa5c56312947a228af6c79a5b54
therealrastie <41759195+therealrastie@users.noreply.github.com>
2018-10-07 01:41:58 -0400

    more

--- happyterror.py
+++ happyterror.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[58]:
+# In[205]:
 
 
 #dependencies
@@ -9,22 +9,42 @@ import pandas as pd
 import numpy as np
 
 
-# In[59]:
+# In[206]:
 
 
-terror_csv ="C:/Users/randy/Desktop/PREWORK/global_terrorism.csv"
+terror_csv ="C:/Users/randy/Desktop/Group_project/terrorism.csv"
 df=pd.read_csv(terror_csv, encoding = "Latin")
+
 df.head()
 
 
-# In[60]:
+# In[207]:
 
 
 new_df = df[["iyear","imonth", "country_txt", "region_txt", "city", "attacktype1_txt", "targtype1_txt","nkill","nwound"]]
 new_df.head()
 
 
-# In[62]:
+# In[208]:
+
+
+df1=new_df.rename(index=str, columns={"iyear": "Year","imonth": "Month" , "country_txt": "Country", "region_txt": "Region" , "city": "City", "attacktype1_txt": "Attack Type" , "targtype1_txt": "Attack Type", "nkill": "Number Killed", "nwound": "Number Wounded"})
+
+
+# In[209]:
+
+
+df_new=df1.dropna()
+df_new
+
+
+# In[215]:
+
+
+df_new.pivot_table(index='Year',values=['Number Killed','Number Wounded'])
+
+
+# In[66]:
 
 
 new_df.dtypes
